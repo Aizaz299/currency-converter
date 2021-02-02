da443731aaed7577e6a4f358e13c71d80430c26d
Aizaz Rehman <aziaz299@gmail.com>
2021-02-02 14:07:39 +0400

    onchange

--- src/App.js
+++ src/App.js
@@ -4,9 +4,26 @@ import CurrencyDropDown from "./CurrencyDropDown";
 
 export default class App extends React.Component{
 
+    state = {currVal:1}
+    onChangeValue=event=>{
+        this.setState({currVal: event.target.value})
+        console.log(this.state.currVal)
+    }
+
     render() {
         return (
-            <div>
+
+            <div className="ui container">
+                 <label> Amount
+                    <input type="text" 
+                    placeholder="Currency"
+                    value={this.state.currVal}
+                    onChange={this.onChangeValue}/>
+                </label>
+                <div className="ui corner label">
+                <i className="money bill alternate outline"></i>
+                 </div>
+                 
             <CurrencyDropDown/>
             <CurrencyDropDown/>    
              </div>
--- src/CurrencyDropDown.js
+++ src/CurrencyDropDown.js
@@ -3,12 +3,16 @@ import React from "react"
 
 const CurrencyDropDown = () =>{
     return(
+      <div className="ui container">
+      <label>
         <select>
-        <option value="grapefruit">Grapefruit</option>
+        <option value="Currency">Grapefruit</option>
         <option value="lime">PKR - Rupees</option>
         <option selected value="coconut">USD- Dollor</option>
         <option value="mango">DXB - Dirham </option>
       </select>
+      </label>
+      </div>
     )
 }
 
